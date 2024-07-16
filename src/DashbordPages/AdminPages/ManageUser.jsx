f9c3a917d29243465322e0e7c82d740c455bf47b
Ajharahmed3218 <nuhash3218@gmail.com>
2024-07-16 20:33:30 +0600

    Added : user pending and comfirm

--- src/DashbordPages/AdminPages/ManageUser.jsx
+++ src/DashbordPages/AdminPages/ManageUser.jsx
@@ -1,13 +1,69 @@
 import { FaUsers } from "react-icons/fa";
 import useAllRequest from "../../Hooks/useAllRequest";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosPublic from "../../Hooks/axiosPublic";
 
 
 const ManageUser = () => {
     const [request, refetch] = useAllRequest();
     console.log(request);
+    // const navigate = useNavigate();
+    // const location = useLocation();
+    const axiosPublic = useAxiosPublic();
+
+
+    const handleConfirm = (data) => {
+        if (data.role === 'user') {
+            data.tk = 40;  // Add new field 'tk' with value 40
+            console.log(data, 'user');
+
+            axiosPublic.post('http://localhost:5000/register', data)
+                .then(res => {
+                    if (res.data.insertedId) {
+                        refetch()
+                        return Swal.fire({
+                            title: 'Success',
+                            text: `Successfully Registered. ${data.name, data.email}`,
+                            icon: 'success',
+                            confirmButtonText: 'Cool'
+                        });
+
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                    Swal.fire({
+                        title: 'Error',
+                        text: error.message,
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                    });
+                });
+        } else {
+            data.tk = 10000;
+            console.log(data, 'Agent');
+            axiosPublic.post('http://localhost:5000/register', data)
+                .then(res => {
+                    if (res.data.insertedId) {
+                        refetch()
+                        return Swal.fire({
+                            title: 'Success',
+                            text: `Successfully Registered. ${data.name, data.email}`,
+                            icon: 'success',
+                            confirmButtonText: 'Cool'
+                        });
+
+                    }
+                })
+
+        }
+    };
+
+
     return (
         <div>
-             {/* <h1>Total user {users?.length}</h1> */}
+            {/* <h1>Total user {users?.length}</h1> */}
             <div className="overflow-x-auto">
                 <table className="table table-zebra">
                     {/* head */}
@@ -17,23 +73,25 @@ const ManageUser = () => {
                             <th>Name</th>
                             <th>email</th>
                             <th>Role</th>
-                            <th>Membership</th>
+                            <th>Confirm</th>
                             <th>Delete</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {
-                            request.map((user, inx) => <tr key={user._id}>
-                                <th>{inx + 1}</th>
-                                <td>{user?.name}</td>
-                                <td>{user?.email}</td>
-                                <td>{user?.rool}</td>
-                                {/* <td>{user?.role === 'admin' ? "Admin" :<button onClick={() => handleMakeAdmin(user._id)} className="btn text-xl bg-[#bdcbf4]"><FaUsers /></button>}</td> */}
-                                {/* <td><button onClick={() => handleDelete(user._id)} className="btn btn-sm bg-red-500 text-white">Delete</button></td> */}
-                            </tr>)
-                        }
+                    {request.length !== 0 ? 
+                        <tbody>
+                            {
+                                request.map((user, inx) => <tr key={user._id}>
+                                    <th>{inx + 1}</th>
+                                    <td>{user?.name}</td>
+                                    <td>{user?.email}</td>
+                                    <td>{user?.role}</td>
+                                    <td>{user?.role === 'admin' ? "Admin" : <button onClick={() => handleConfirm(user)} className="btn text-xl bg-[#bdcbf4]"><FaUsers /></button>}</td>
+                                    {/* <td><button onClick={() => handleDelete(user._id)} className="btn btn-sm bg-red-500 text-white">Delete</button></td> */}
+                                </tr>)
+                            }
 
-                    </tbody>
+                        </tbody> : <div className="text-4xl flex justify-center items-center"> <h1>NO DATA</h1> </div>
+                    }
                 </table>
             </div>
         </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import useAxiosPublic from "../../Hooks/axiosPublic";
+import useAllRequest from "../../Hooks/useAllRequest";
 
 const Register = () => {
-    const { createUser, setUser, upDateUserProfile } = useContext(AuthContext);
+    const { createUser, setUser, upDateUserProfile, user } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     const axiosPublic = useAxiosPublic();
+
+    // tenstak
+    const [request] = useAllRequest();
+    const [role, setRole] = useState("user");
+
+    const handleRoleChange = (e) => {
+        setRole(e.target.value);
+    };
 
     const handleLogin = e => {
         e.preventDefault();
@@ -18,8 +27,7 @@ const Register = () => {
         const phone = form.phone.value;
         const photo = form.photo.value;
         const password = form.password.value;
-        const rool = "user";
-        console.log(email, password, name, photo, phone);
+        console.log(email, password, name, photo, phone, role);
 
         if (!/^\d{5}$/.test(password)) {
             return Swal.fire({
@@ -39,9 +47,9 @@ const Register = () => {
                         const userInfo = {
                             name,
                             email,
-                            rool,
+                            role,
                             phone,
-                            password: newPass // Ensure this matches backend field
+                            password: newPass, // Ensure this matches backend field
                         };
 
                         axiosPublic.post('http://localhost:5000/request', userInfo)
@@ -81,56 +89,86 @@ const Register = () => {
             });
     };
 
+    const isRequest = request.find(users => users?.email === user?.email);
+    console.log(isRequest?.email, user, request);
+
     return (
         <div>
             <div className="hero md:min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="md:mr-12 md:w-1/2 w-2/3">
-                        <img src="smart-login.jpg" alt="" />
+                {isRequest?.email ? (
+                    <div className="relative m-16 lg:mt-1 mt-28">
+                        <button className="absolute py-1 px-3 -left-8 -top-2 -rotate-[10deg] border border-black black_border bg-[#7e22ce] text-white font-bold">
+                            WARNING!
+                        </button>
+
+                        <div className="purple_border p-8 border border-black">
+                            The
+                            <span className="font-mono text-purple-700 font-bold">Request</span>
+                            is Pending
+                        </div>
                     </div>
-                    <div className="card card-body shrink-0 md:w-full  md:max-w-sm shadow-2xl bg-slate-400">
-                        <h1 className="text-4xl font-semibold text-center mt-5">Register</h1>
-                        <form onSubmit={handleLogin} className="">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Name</span>
-                                </label>
-                                <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Photo Url</span>
-                                </label>
-                                <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Phone</span>
-                                </label>
-                                <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Register</button>
-                            </div>
-                        </form>
-                        <p className="my-4 text-center">Already Have an account? <Link className="text-[#FF3811] font-bold" to={"/login"}>Login</Link> </p>
+                ) : (
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <div className="md:mr-12 md:w-1/2 w-2/3">
+                            <img src="smart-login.jpg" alt="" />
+                        </div>
+                        <div className="card card-body shrink-0 md:w-full md:max-w-sm shadow-2xl bg-slate-400">
+                            <h1 className="text-4xl font-semibold text-center mt-5">Register</h1>
+                            <form onSubmit={handleLogin}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Photo Url</span>
+                                    </label>
+                                    <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Phone</span>
+                                    </label>
+                                    <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Role</span>
+                                    </label>
+                                    <label className="cursor-pointer">
+                                        <input type="radio" name="role" value="user" checked={role === "user"} onChange={handleRoleChange} className="radio radio-primary" />
+                                        <span className="label-text ml-2">User</span>
+                                    </label>
+                                    <label className="cursor-pointer">
+                                        <input type="radio" name="role" value="agent" checked={role === "agent"} onChange={handleRoleChange} className="radio radio-primary" />
+                                        <span className="label-text ml-2">Agent</span>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Register</button>
+                                </div>
+                            </form>
+                            <p className="my-4 text-center">Already Have an account? <Link className="text-[#FF3811] font-bold" to={"/login"}>Login</Link> </p>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
